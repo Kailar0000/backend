0bc9af61c074ff243a06701ea969595a839f9362
Nazarenko <114772769+Kailar0000@users.noreply.github.com>
2024-02-27 06:15:04 +0300

    Update urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,7 +5,7 @@ from .views import recipes_list, recipe_page, new_recipe, update_recipe
 
 urlpatterns = [
     path('', recipes_list, name='home'),
-    path('recipe/<int:recipe_pk>/', recipe_page, name='recipe_detail'),
+    path('recipe/<int:recipe_pk>/', recipe_page, name='recipe_page'),
     path('newrecipe', new_recipe, name='new_recipe'),
     path('updaterecipe', update_recipe, name='new_recipe'),
 ]
